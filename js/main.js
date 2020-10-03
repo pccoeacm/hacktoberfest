52da2b3b88e0b7cfe3ce029297ce8a29ba270cc4
palakg01 <palakgupta6506@gmail.com>
2020-10-03 16:02:22 +0530

    add back-to-top button

--- js/main.js
+++ js/main.js
@@ -18,3 +18,21 @@ counters.forEach((counter) => {
 
   updateCount();
 });
+
+// When the user scrolls down 20px from the top of the document, show the button
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
